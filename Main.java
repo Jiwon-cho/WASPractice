c803267e6501fe2019183a05d454fbb2fc61ee9c
chojiwon <gwon188@gmail.com>
2021-06-05 23:14:39 +0900

    WAS

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.member.run;
+
+import com.member.controller.MemberController;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new MemberController().MainMenu();
+	}
+
+}
